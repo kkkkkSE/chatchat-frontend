9053e1d2fade4346f633d1d9940829bc1529a1d6
kkkkkse <eun2abc@gmail.com>
2023-06-08 18:13:30 +0900

    [setting] 프로젝트 세팅

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen} from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+	it('renders greeting message', () => {
+		render(<App />);
+
+		screen.getByText(/Hello/);
+	});
+});
