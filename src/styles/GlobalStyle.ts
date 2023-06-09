2d427deec7685e76620443dd82c37cf144f8b642
kkkkkse <eun2abc@gmail.com>
2023-06-09 18:14:36 +0900

    [chore] 글로벌 스타일, 테마 스타일 세팅 및 타입 지정

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,63 @@
+import {createGlobalStyle} from 'styled-components';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+        font-family: 'Gmarket Sans';
+        src: url('../assets/font/GmarketSansBold.woff2')
+						 url('../assets/font/GmarketSansBold.woff');
+				font-weight: bold;
+  }
+	@font-face {
+        font-family: 'Gmarket Sans';
+        src: url('../assets/font/GmarketSansMedium.woff2')
+        		 url('../assets/font/GmarketSansMedium.woff');
+				font-weight: normal;
+  }
+	@font-face {
+        font-family: 'Noto Sans KR';
+        src: url('../assets/font/NotoSansKR-Bold.woff2')
+						 url('../assets/font/NotoSansKR-Bold.woff');
+				font-weight: bold;
+  }
+	@font-face {
+        font-family: 'Noto Sans KR';
+        src: url('../assets/font/NotoSansKR-Regular.woff2')
+						 url('../assets/font/NotoSansKR-Regular.woff');
+				font-weight: normal;
+  }
+	
+
+	html {
+			box-sizing: border-box;
+	}
+			
+	*,
+	*::before,
+	*::after {
+			box-sizing: inherit;
+	}
+			
+	html {
+			font-size: 62.5%;
+	}
+			
+	body {
+			font-family: 'Noto Sans KR';
+			color: ${props => props.theme.colors.black.default};
+			line-height: 1.5;
+			${props => props.theme.texts.regular.medium};
+	}
+	
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	button {
+		outline: none;
+		border: none;
+	}
+`;
+
+export default GlobalStyle;
