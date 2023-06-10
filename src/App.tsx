e70ef8f79c065e1f9613ce3e6143b5c3d34f22cb
kkkkkse <eun2abc@gmail.com>
2023-06-10 15:26:06 +0900

    [design] Reset CSS, Global Style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import {ThemeProvider} from 'styled-components';
+import {Reset} from 'styled-reset';
+
+import defaultTheme from './styles/defaultTheme';
+import GlobalStyle from './styles/GlobalStyle';
+
+import Button from './components/ui/Button';
+import TextBox from './components/ui/TextBox';
+
 export default function App() {
 	return (
-		<p>Hello, world!</p>
+		<ThemeProvider theme={defaultTheme}>
+			<Reset />
+			<GlobalStyle />
+		</ThemeProvider>
 	);
 }
