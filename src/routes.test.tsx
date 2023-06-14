d5565f523674355b43eb235343aa3b227ce2bafa
kkkkkse <eun2abc@gmail.com>
2023-06-14 17:33:20 +0900

    [test] 로그인 전단계 페이지 라우트 테스트 추가

--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -1,13 +1,19 @@
 import {render, screen} from '@testing-library/react';
 import {createMemoryRouter, RouterProvider} from 'react-router';
+import {ThemeProvider} from 'styled-components';
 
 import routes from './routes';
+import defaultTheme from './styles/defaultTheme';
 
 const context = describe;
 
 const setupRouterProvider = (path: string) => {
 	const router = createMemoryRouter(routes, {initialEntries: [path]});
-	render((<RouterProvider router={router} />));
+	render(
+		<ThemeProvider theme={defaultTheme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>,
+	);
 };
 
 describe('routes', () => {
@@ -15,7 +21,25 @@ describe('routes', () => {
 		it('renders <HomePage />', () => {
 			setupRouterProvider('/');
 
-			screen.getByText(/type/);
+			screen.getByText(/회원 유형을 선택해주세요/);
+		});
+	});
+
+	context('when the current path is “/login”', () => {
+		it('renders <HomePage />', () => {
+			setupRouterProvider('/login');
+
+			screen.getByLabelText(/아이디/);
+			screen.getByRole('button', {name: /로그인/});
+		});
+	});
+
+	context('when the current path is “/sign-up', () => {
+		it('renders <HomePage />', () => {
+			setupRouterProvider('/sign-up');
+
+			screen.getByLabelText(/비밀번호 확인/);
+			screen.getByRole('button', {name: /가입하기/});
 		});
 	});
 });
