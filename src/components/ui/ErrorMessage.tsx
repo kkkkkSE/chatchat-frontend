15ba47ecccb2cb9cdf2f33311d0cdc1537e3289f
kkkkkse <eun2abc@gmail.com>
2023-06-14 16:45:44 +0900

    [design] 에러 메세지 UI 컴포넌트 생성

---
+++ src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+type ErrorMessageProps = {
+	children: string;
+};
+function ErrorMessage({children}: ErrorMessageProps) {
+	return (
+		<StyledErrorMessage>
+			{children}
+		</StyledErrorMessage>
+	);
+}
+
+export default ErrorMessage;
+
+const StyledErrorMessage = styled.p`
+    ${props => props.theme.texts.regular.small}
+    color: ${props => props.theme.colors.accent.default};
+    padding-block: 1.2rem;
+
+    @media screen and (${props => props.theme.breakPoint.mobile}){
+      ${props => props.theme.texts.regular.hint}
+      padding-block: 1rem;
+    }
+`;
