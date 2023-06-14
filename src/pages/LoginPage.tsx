b11a0c172fcc936999b19c16250f4098f9ec9e9f
kkkkkse <eun2abc@gmail.com>
2023-06-14 16:46:41 +0900

    [design] 로그인, 회원가입 화면 구현(기능 X)

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,31 @@
+import {useLocation, useNavigate} from 'react-router-dom';
+import Button from '../components/ui/Button';
+import ErrorMessage from '../components/ui/ErrorMessage';
+import TextBox from '../components/ui/TextBox';
+
+function LoginPage() {
+	const navigate = useNavigate();
+
+	const location = useLocation();
+	const queryString = location.search;
+
+	const handleClickSignUp = () => {
+		navigate('/sign-up' + queryString);
+	};
+
+	return (
+		<div>
+			<TextBox label={'아이디'} value={''} />
+			<TextBox label={'비밀번호'} value={''} />
+			<Button
+				size='fit'
+				color='gray'
+				onClick={handleClickSignUp}
+				marginTop
+			>회원가입</Button>
+			<Button marginTop>로그인</Button>
+		</div>
+	);
+}
+
+export default LoginPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,13 @@
 import PreLoginLayout from './components/PreLoginLayout';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 const routes = [
 	{element: <PreLoginLayout />, children: [
 		{path: '/', element: <HomePage />},
+		{path: '/login', element: <LoginPage />},
+		{path: '/sign-up', element: <SignUpPage />},
 	]},
 ];
 
