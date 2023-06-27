ea1648a713087a5219bfba2a06d6e567e6d50f29
kkkkkse <eun2abc@gmail.com>
2023-06-27 23:20:39 +0900

    [fix] jest image import 에러 해결

--- jest.config.js
+++ jest.config.js
@@ -24,4 +24,8 @@ module.exports = {
     '<rootDir>/node_modules/',
     '<rootDir>/dist/',
   ],
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
 };
