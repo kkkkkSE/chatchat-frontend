fdd5627e52f92feb7054ec5652cd2af8a91980f9
kkkkkse <eun2abc@gmail.com>
2023-08-18 15:57:47 +0900

    [chore] pages 폴더 index 파일 생성

---
+++ src/pages/index.ts
@@ -0,0 +1,23 @@
+import ChatListPage from './ChatListPage';
+import ChatRoomPage from './ChatRoomPage';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import ProfileEditPage from './ProfileEditPage';
+import ProfilePage from './ProfilePage';
+import SignUpPage from './SignUpPage';
+import AutoReplyAdminPage from './AutoReplyAdminPage';
+import AutoReplyNewPage from './AutoReplyNewPage';
+import AutoReplyEditPage from './AutoReplyEditPage';
+
+export default {
+  ChatListPage,
+  ChatRoomPage,
+  HomePage,
+  LoginPage,
+  ProfileEditPage,
+  ProfilePage,
+  SignUpPage,
+  AutoReplyAdminPage,
+  AutoReplyNewPage,
+  AutoReplyEditPage,
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,35 +3,26 @@ import PreLoginLayout from './components/layout/PreLoginLayout';
 
 import { STATIC_ROUTES } from './constants/routes';
 
-import ChatListPage from './pages/ChatListPage';
-import ChatRoomPage from './pages/ChatRoomPage';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ProfileEditPage from './pages/ProfileEditPage';
-import ProfilePage from './pages/ProfilePage';
-import SignUpPage from './pages/SignUpPage';
-import AutoReplyAdminPage from './pages/AutoReplyAdminPage';
-import AutoReplyNewPage from './pages/AutoReplyNewPage';
-import AutoReplyEditPage from './pages/AutoReplyEditPage';
+import pages from './pages';
 
 const routes = [
   {
     element: <PreLoginLayout />,
     children: [
-      { path: STATIC_ROUTES.HOME, element: <HomePage /> },
-      { path: STATIC_ROUTES.LOGIN, element: <LoginPage /> },
-      { path: STATIC_ROUTES.SIGN_UP, element: <SignUpPage /> },
+      { path: STATIC_ROUTES.HOME, element: <pages.HomePage /> },
+      { path: STATIC_ROUTES.LOGIN, element: <pages.LoginPage /> },
+      { path: STATIC_ROUTES.SIGN_UP, element: <pages.SignUpPage /> },
     ],
   }, {
     element: <PostLoginLayout />,
     children: [
-      { path: STATIC_ROUTES.CHATROOMS, element: <ChatListPage /> },
-      { path: `${STATIC_ROUTES.CHATROOMS}/:id`, element: <ChatRoomPage /> },
-      { path: STATIC_ROUTES.MY_PROFILE, element: <ProfilePage /> },
-      { path: STATIC_ROUTES.MY_PROFILE_EDIT, element: <ProfileEditPage /> },
-      { path: STATIC_ROUTES.AUTO_REPLIES, element: <AutoReplyAdminPage /> },
-      { path: STATIC_ROUTES.AUTO_REPLIES_NEW, element: <AutoReplyNewPage /> },
-      { path: `${STATIC_ROUTES.AUTO_REPLIES}/:id`, element: <AutoReplyEditPage /> },
+      { path: STATIC_ROUTES.CHATROOMS, element: <pages.ChatListPage /> },
+      { path: `${STATIC_ROUTES.CHATROOMS}/:id`, element: <pages.ChatRoomPage /> },
+      { path: STATIC_ROUTES.MY_PROFILE, element: <pages.ProfilePage /> },
+      { path: STATIC_ROUTES.MY_PROFILE_EDIT, element: <pages.ProfileEditPage /> },
+      { path: STATIC_ROUTES.AUTO_REPLIES, element: <pages.AutoReplyAdminPage /> },
+      { path: STATIC_ROUTES.AUTO_REPLIES_NEW, element: <pages.AutoReplyNewPage /> },
+      { path: `${STATIC_ROUTES.AUTO_REPLIES}/:id`, element: <pages.AutoReplyEditPage /> },
     ],
   },
 ];
