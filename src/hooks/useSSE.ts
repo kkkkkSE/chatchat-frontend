03ea49c6e5f8d2fedaf72fd1c3eeefe18291b287
kkkkkse <eun2abc@gmail.com>
2023-08-28 15:01:04 +0900

    [feat] 채팅 목록 실시간 갱신을 위한 SSE 연결

---
+++ src/hooks/useSSE.ts
@@ -0,0 +1,51 @@
+import { useEffect } from 'react';
+
+import { useQueryClient } from '@tanstack/react-query';
+
+import { EventSourcePolyfill } from 'event-source-polyfill';
+
+import { QUERY_KEY } from '../constants/reactQuery';
+
+import useAccessToken from './useAccessToken';
+import useLoginUserStore from './useLoginUserStore';
+
+const useSSE = () => {
+  const queryClient = useQueryClient();
+
+  const { accessToken } = useAccessToken();
+
+  const [{ userType }] = useLoginUserStore();
+
+  useEffect(() => {
+    const eventSource = new EventSourcePolyfill(
+      `${process.env.API_BASE_URL}/connect?userType=${userType}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    eventSource.addEventListener('sse', (event: any) => {
+      if (!event?.data) {
+        return;
+      }
+
+      queryClient.invalidateQueries(QUERY_KEY.CHAT_LIST);
+    });
+
+    eventSource.addEventListener('error', (event: any) => {
+      if (event.error.message.startsWith('No activity within')) {
+        return;
+      }
+
+      eventSource.close();
+    });
+
+    return (() => {
+      eventSource.close();
+    });
+  }, [accessToken]);
+};
+
+export default useSSE;
--- src/pages/ChatListPage.tsx
+++ src/pages/ChatListPage.tsx
@@ -1,7 +1,11 @@
+import useSSE from '../hooks/useSSE';
+
 import ContentLayout from '../components/layout/ContentLayout';
 import ChatList from '../components/chat/ChatList';
 
 export default function ChatListPage() {
+  useSSE();
+
   return (
     <ContentLayout
       pageHeader="채팅 목록"
